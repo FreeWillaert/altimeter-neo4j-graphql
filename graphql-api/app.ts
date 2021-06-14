c50a9eb189dda284df5cc259fc8db62401704012
fwillaerlpu-at-634087270915 <frederik.willaert@gmail.com>
2021-06-14 19:38:05 +0200

    create actual graphql lambda

--- graphql-api/app.ts
+++ graphql-api/app.ts
@@ -1,8 +1,11 @@
 import * as fs from 'fs'
+import * as _ from 'lodash'
+
 import * as neo4j from 'neo4j-driver'
+import { makeAugmentedSchema, neo4jgraphql } from 'neo4j-graphql-js'
+import { ApolloServer } from 'apollo-server-lambda'
 
 
-// const { ApolloServer } = require("apollo-server");
 
 // TODO: Read from env vars?
 const config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
@@ -14,6 +17,111 @@ const driver = neo4j.driver(
     neo4j.auth.basic(config.neo4j.user, config.neo4j.password)
 );
 
-export function handler(): string {
-  return `Hello world!`;
-}
+// TODO: Move to separate module
+const resolvers = {
+    ec2__instance: {
+        // dummy(parent) {
+        //     return `${parent.field1} ${parent.field2}`; // TODO: This only works if field1 and field2 are also requested by the query!
+        // },
+    },
+    Query: {
+        internet_exposed_ec2_instances: async (parent:any, args:any, context:any, info:any) => {
+            const internet_exposed_filter = {
+                OR: [
+                    {
+                        security_groups_some: {
+                            ingress_rules_some: {
+                                ip_ranges_some: {
+                                    alti__cidr_ip_starts_with: '0.0.0.0'
+                                }
+                            }
+                        }
+                    },
+                    {
+                        network_interfaces_some: {
+                            security_groups_some: {
+                                ingress_rules_some: {
+                                    ip_ranges_some: {
+                                        alti__cidr_ip_starts_with: "0.0.0.0"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+
+            args.filter = (args.filter) ? { AND: [ args.filter, internet_exposed_filter]} : internet_exposed_filter
+
+            return await neo4jgraphql(parent, args, context, info)
+        },
+        internet_exposed_elb_loadbalancers: async (parent:any, args:any, context:any, info:any) => {
+
+            const internet_exposed_filter = {
+                AND: [
+                    { alti__scheme: "internet-facing" },
+                    {
+                        security_groups_some: {
+                            ingress_rules_some: {
+                                ip_ranges_some: {
+                                    alti__cidr_ip_starts_with: '0.0.0.0'
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+
+            args.filter = (args.filter) ? { AND: [ args.filter, internet_exposed_filter]} : internet_exposed_filter
+
+            return await neo4jgraphql(parent, args, context, info)
+        },
+        internet_exposed_elbv2_loadbalancers: async (parent:any, args:any, context:any, info:any) => {
+
+            const internet_exposed_filter = {
+                AND: [
+                    { alti__scheme: "internet-facing" },
+                    {
+                        security_groups_some: {
+                            ingress_rules_some: {
+                                ip_ranges_some: {
+                                    alti__cidr_ip_starts_with: '0.0.0.0'
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+
+            args.filter = (args.filter) ? { AND: [ args.filter, internet_exposed_filter]} : internet_exposed_filter
+
+            return await neo4jgraphql(parent, args, context, info)
+        },
+        sample_my_resources: async (parent:any, args:any, context:any, info:any) => {
+            let session
+            try {
+                session = driver.session()
+                const result = await session.run('MATCH (n:Resource) RETURN n')
+                const fields = result.records.map(record => _.merge({ id: +record.get(0).identity }, record.get(0).properties))
+                return fields
+            } finally {
+                if (session) await session.close()
+            }
+        },
+        sample_regions_with_ec2: async (parent:any, args:any, context:any, info:any) => {
+            const allRegions = await neo4jgraphql(parent, args, context, info)
+            const nonEmptyRegions = allRegions.filter((f:any) => f.ec2__instances.length > 0)
+
+            return nonEmptyRegions
+        }
+    }
+};
+
+const schema = makeAugmentedSchema({ typeDefs, resolvers })
+
+const server = new ApolloServer({
+    schema,
+    context: { driver }
+});
+
+exports.graphqlHandler = server.createHandler();
