6d103a2f16c427d66864b017d4effa28fd841951
fwillaerlpu-at-634087270915 <frederik.willaert@gmail.com>
2021-06-16 12:18:49 +0200

    prepare for cors

--- scanner/stacks/graphql_api_stack.py
+++ scanner/stacks/graphql_api_stack.py
@@ -7,7 +7,7 @@ from aws_cdk import aws_lambda_nodejs
 from aws_cdk.aws_ec2 import IInstance, IVpc, SubnetSelection
 from aws_cdk.aws_secretsmanager import ISecret
 from aws_cdk.aws_lambda_nodejs import ICommandHooks, NodejsFunction, BundlingOptions
-from aws_cdk.aws_apigateway import ApiKeySourceType, LambdaRestApi
+from aws_cdk.aws_apigateway import ApiKeySourceType, Cors, CorsOptions, LambdaRestApi
 class GraphqlApiStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, config, vpc: IVpc, instance: IInstance, neo4j_user_secret: ISecret, **kwargs) -> None:
@@ -39,14 +39,17 @@ class GraphqlApiStack(cdk.Stack):
 
         api = LambdaRestApi(self, 'apigateway-api-altimeter-graphql',
             handler=graphql_api_function,            
-            proxy=False,
-            
+            proxy=False
         )
 
         key = api.add_api_key('default');
 
-        # TODO: try without these?
-        items = api.root.add_resource('graphql');
+        items = api.root.add_resource('graphql',
+            default_cors_preflight_options=CorsOptions(
+                allow_origins=Cors.ALL_ORIGINS, # TODO: Limit to GUI?
+                allow_methods=['GET','POST']
+            )
+        )
         items.add_method('GET', api_key_required=True)
         items.add_method('POST', api_key_required=True)
 
